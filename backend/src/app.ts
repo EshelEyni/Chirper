eaef56c40023d7d3d4490c06b69a630846e51e0d
Eshel Eyni <esheleyni@gmail.com>
2023-05-09 22:48:55 +0300

    refactored app file and added unhandled router error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,20 @@
+import { NextFunction, Request, Response } from "express";
 const express = require("express");
 const path = require("path");
 const setupAsyncLocalStorage = require("./middlewares/setupAls.middleware");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
 const app = express();
 
 // Express App Config
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static("public"));
 
 // cors
 if (process.env.NODE_ENV === "production") {
-  // Express serve static files on production environment
   app.use(express.static(path.resolve(__dirname, "public")));
 } else {
-  // Configuring CORS
   const corsOptions = {
-    // Make sure origin contains the url your frontend is running on
     origin: [
       "http://127.0.0.1:8080",
       "http://localhost:8080",
@@ -47,8 +43,15 @@ app.use("/api/location", locationRoutes());
 // app.use("/api/auth", authRoutes);
 // setupSocketAPI(http);
 
-app.get("/**", (req: any, res: { sendFile: (arg0: any) => void }) => {
+app.get("/**", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
 });
 
 module.exports = app;

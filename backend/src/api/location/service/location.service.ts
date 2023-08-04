d0014d2fba2515e15d508c39efe19d477877d2a9
Eshel Eyni <esheleyni@gmail.com>
2023-08-04 15:38:34 +0300

    Backend - Last Stage - Location Service Testing

--- backend/src/api/location/service/location.service.ts
+++ backend/src/api/location/service/location.service.ts
@@ -1,16 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import config from "../../../config";
-import { createClient } from "@google/maps";
+import {
+  ClientResponse,
+  GeocodingResult,
+  PlaceAutocompleteResult,
+  PlaceSearchResponse,
+  PlaceSearchResult,
+  ReverseGeocodingResponse,
+  createClient,
+} from "@google/maps";
 
 import { Location } from "../../../../../shared/interfaces/location.interface";
+
+type resultType = PlaceAutocompleteResult | PlaceSearchResult | GeocodingResult;
 
 const googleMapsClient = createClient({
   key: config.googleApiKey || "",
   Promise: Promise,
 });
-
-const englishRegex = /^[A-Za-z0-9\s.,!?@()_\\-]+$/;
-const plusSignRegex = /\+/;
 
 async function getUserSurroundingLocations(lat: number, lng: number): Promise<Location[]> {
   const currLoaction = await _getCurrUserLocation(lat, lng);
@@ -21,48 +27,8 @@ async function getUserSurroundingLocations(lat: number, lng: number): Promise<Lo
       language: "en",
     })
     .asPromise();
-
-  const { results } = response.json;
-  const locations = results
-    .map((location: any) => {
-      return {
-        name: location.name,
-        placeId: location.place_id,
-        lat: location.geometry.location.lat,
-        lng: location.geometry.location.lng,
-      };
-    })
-    .filter((location: any) => englishRegex.test(location.name));
-
+  const locations = _getLocationFromResponse({ response }) as Location[];
   return currLoaction ? [currLoaction, ...locations.slice(0, 6)] : locations.slice(0, 7);
-}
-
-async function _getCurrUserLocation(lat: number, lng: number): Promise<Location | null> {
-  const response = await googleMapsClient
-    .reverseGeocode({
-      latlng: { lat, lng },
-      language: "en",
-    })
-    .asPromise();
-
-  const { results } = response.json;
-  const result = results.find((location: any) => {
-    return (
-      englishRegex.test(location.formatted_address) &&
-      !plusSignRegex.test(location.formatted_address)
-    );
-  });
-
-  if (!result) return null;
-
-  const location = {
-    name: result.formatted_address,
-    placeId: result.place_id,
-    lat: result.geometry.location.lat,
-    lng: result.geometry.location.lng,
-  };
-
-  return location;
 }
 
 async function getLocationBySearchTerm(searchTerm: string): Promise<Location[]> {
@@ -73,16 +39,65 @@ async function getLocationBySearchTerm(searchTerm: string): Promise<Location[]> 
       types: "(cities)",
     })
     .asPromise();
-  const res = response.json.predictions;
-  const locations: Location[] = res.map((location: any) => {
-    return {
-      name: location.description as string,
-      placeId: location.place_id as string,
-      lat: 0,
-      lng: 0,
-    };
-  });
+  const { predictions } = response.json;
+  const locations: Location[] = predictions.map(_formatResultToLocationObj);
+  await _setCoordsToPrecditedLocations(locations);
+  return locations;
+}
 
+async function _getCurrUserLocation(lat: number, lng: number): Promise<Location | null> {
+  const response = await googleMapsClient
+    .reverseGeocode({
+      latlng: { lat, lng },
+      language: "en",
+    })
+    .asPromise();
+  return _getLocationFromResponse({ response, isSingleResult: true }) as Location;
+}
+
+function _getLocationFromResponse({
+  response,
+  isSingleResult = false,
+}: {
+  response: ClientResponse<PlaceSearchResponse | ReverseGeocodingResponse>;
+  isSingleResult?: boolean;
+}): Location[] | Location | null {
+  const englishRegex = /^[A-Za-z0-9\s.,!?@()_\\-]+$/;
+  const plusSignRegex = /\+/;
+  const { results } = response.json;
+  if (isSingleResult) {
+    const result = (results as GeocodingResult[]).find(
+      (location: GeocodingResult) =>
+        englishRegex.test(location.formatted_address) &&
+        !plusSignRegex.test(location.formatted_address)
+    );
+    return result ? _formatResultToLocationObj(result) : null;
+  }
+  return (results as PlaceSearchResult[])
+    .filter((location: PlaceSearchResult) => englishRegex.test(location.name))
+    .map(_formatResultToLocationObj);
+}
+
+function _formatResultToLocationObj(result: resultType): Location {
+  const isGeocodingResult = (obj: resultType): obj is GeocodingResult => "formatted_address" in obj;
+  const isPlaceAutocompleteResult = (obj: resultType): obj is PlaceAutocompleteResult =>
+    "description" in obj;
+  let name: string, lat: number, lng: number;
+  if (isGeocodingResult(result)) name = result.formatted_address;
+  else if (isPlaceAutocompleteResult(result)) name = result.description;
+  // eslint-disable-next-line prefer-destructuring
+  else name = result.name;
+
+  const { place_id } = result;
+  if (isPlaceAutocompleteResult(result)) lat = lng = 0;
+  else {
+    const { lat: resLat, lng: resLng } = result.geometry.location;
+    (lat = resLat), (lng = resLng);
+  }
+  return { name, placeId: place_id, lat: lat, lng: lng };
+}
+
+async function _setCoordsToPrecditedLocations(locations: Location[]) {
   for (const location of locations) {
     const response = await googleMapsClient
       .place({
@@ -90,12 +105,9 @@ async function getLocationBySearchTerm(searchTerm: string): Promise<Location[]> 
         language: "en",
       })
       .asPromise();
-    const res = response.json.result;
-    location.lat = res.geometry.location.lat;
-    location.lng = res.geometry.location.lng;
+    const { lat: resLat, lng: resLng } = response.json.result.geometry.location;
+    (location.lat = resLat), (location.lng = resLng);
   }
-
-  return locations;
 }
 
 export default {

79dfbee2dca2bf729f3ddf4113f63bc8dfb0cf14
Eshel Eyni <esheleyni@gmail.com>
2023-08-29 02:07:47 +0300

    Backend - Youtube SDK

--- backend/src/api/bot/services/youtube/youtube.service.ts
+++ backend/src/api/bot/services/youtube/youtube.service.ts
@@ -1,13 +1,29 @@
-import axios from "axios";
 import { AppError } from "../../../../services/error/error.service";
+import { google } from "googleapis";
 require("dotenv").config();
+
+const apiKey = process.env.GOOGLE_API_KEY;
+if (!apiKey) throw new AppError("GOOGLE_API_KEY is undefined", 500);
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: apiKey,
+});
 
 async function getYoutubeVideo(prompt: string): Promise<string> {
   const apiKey = process.env.GOOGLE_API_KEY;
   if (!apiKey) throw new AppError("GOOGLE_API_KEY is undefined", 500);
 
-  const apiStr = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoCategoryId=10&videoEmbeddable=true&type=video&maxResults=1&key=${apiKey}&q=${prompt}`;
-  const response = await axios.get(apiStr);
+  // TODO: play around with the parameters
+  // const apiStr = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoCategoryId=10&videoEmbeddable=true&type=video&maxResults=1&key=${apiKey}&q=${prompt}`;
+
+  const response = await youtube.search.list({
+    part: ["snippet"],
+    q: prompt,
+    maxResults: 1,
+    type: ["video"],
+  });
+
   const songs = response.data.items;
   const videoUrl = `https://www.youtube.com/watch?v=${songs[0].id.videoId}`;
 
